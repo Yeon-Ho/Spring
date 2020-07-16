99cff950d240ed3169d4fd238072a21c316e4151
Yeon-Ho <65996127+Yeon-Ho@users.noreply.github.com>
2020-07-16 18:02:21 +0900

    Add files via upload

---
+++ SpringFramework/Simple/src/main/java/di/autowired/Car.java
@@ -0,0 +1,32 @@
+package di.autowired;
+
+import javax.annotation.Resource;
+import javax.inject.Inject;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+
+import di.tire.Tire;
+
+public class Car {
+
+	@Qualifier("gTire")
+//	@Resource
+//	@Autowired
+	@Inject
+	private Tire tire;
+	
+	public String getInfo() {
+		return tire.getProduct()+ "장착함!!";
+	}
+
+	public Tire getTire() {
+		return tire;
+	}
+
+	public void setTire(Tire tire) {
+		this.tire = tire;
+	}
+
+	
+}
